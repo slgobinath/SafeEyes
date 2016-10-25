a1cb9920470bbe1152e24f6ed177908ee418358f
Gobinath <slgobinath@gmail.com>
2016-10-25 19:54:35 +0530

    Fixing packaging commands

--- build/arch/build.sh
+++ build/arch/build.sh
@@ -3,13 +3,12 @@
 cd ./pkg
 
 # Create parent directories
-mkdir -p opt/safeeyes
+mkdir -p opt
 mkdir usr
 
 # Copy source files
 cp -r ../../../safeeyes/share ./usr/
-cp -r ../../../safeeyes/safeeyes ./opt/safeeyes/
-cp -r ../../../safeeyes/share/applications/safeeyes.desktop ./opt/safeeyes/
+cp -r ../../../safeeyes/safeeyes ./opt/
 
 # # Create the package
 # tar -cf - .PKGINFO * | xz -c -z - > ../safeeyes.pkg.tar.xz
