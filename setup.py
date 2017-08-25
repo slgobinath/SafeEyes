a6992d2f2aa90b6dd4fd860bc2ce99d6ef4b27e1
Radek SPRTA <mail@radeksprta.eu>
2017-08-25 22:21:19 +0200

    Revert "Make pyaudio optional"
    This reverts commit cc597cf01ef54fb7c558456b35b5a401e2e6f5d4.

--- setup.py
+++ setup.py
@@ -4,17 +4,16 @@ import setuptools
 
 
 requires = [
-    'python-xlib',
-    'pyaudio',
-    'psutil',
-    'babel']
+            'python-xlib',
+            'pyaudio',
+            'psutil',
+            'babel']
 
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 with open(os.path.join(here, 'README.md')) as f:
     long_description = '\n' + f.read()
-
 
 def _data_files(path):
     for root, dirs, files in os.walk(path):
@@ -38,30 +37,18 @@ setuptools.setup(
                                'glade/*.glade',
                                'resource/*']},
     data_files=[('/usr/share/applications', ['share/applications/safeeyes.desktop']),
-                ('/usr/share/icons/hicolor/16x16/apps',
-                 ['share/icons/hicolor/16x16/apps/safeeyes.png']),
-                ('/usr/share/icons/hicolor/24x24/apps',
-                 ['share/icons/hicolor/24x24/apps/safeeyes.png']),
-                ('/usr/share/icons/hicolor/48x48/apps',
-                 ['share/icons/hicolor/48x48/apps/safeeyes.png']),
-                ('/usr/share/icons/hicolor/32x32/apps',
-                 ['share/icons/hicolor/32x32/apps/safeeyes.png']),
-                ('/usr/share/icons/hicolor/64x64/apps',
-                 ['share/icons/hicolor/64x64/apps/safeeyes.png']),
-                ('/usr/share/icons/hicolor/128x128/apps',
-                 ['share/icons/hicolor/128x128/apps/safeeyes.png']),
-                ('/usr/share/icons/hicolor/48x48/status',
-                 ['share/icons/hicolor/48x48/status/safeeyes_enabled.png', 'share/icons/hicolor/48x48/status/safeeyes_disabled.png']),
-                ('/usr/share/icons/hicolor/32x32/status',
-                 ['share/icons/hicolor/32x32/status/safeeyes_enabled.png', 'share/icons/hicolor/32x32/status/safeeyes_disabled.png']),
-                ('/usr/share/icons/hicolor/24x24/status', ['share/icons/hicolor/24x24/status/safeeyes_enabled.png',
-                                                           'share/icons/hicolor/24x24/status/safeeyes_disabled.png', 'share/icons/hicolor/24x24/status/safeeyes_timer.png']),
-                ('/usr/share/icons/hicolor/16x16/status', ['share/icons/hicolor/16x16/status/safeeyes_enabled.png',
-                                                           'share/icons/hicolor/16x16/status/safeeyes_disabled.png', 'share/icons/hicolor/16x16/status/safeeyes_timer.png'])
+                ('/usr/share/icons/hicolor/16x16/apps', ['share/icons/hicolor/16x16/apps/safeeyes.png']),
+                ('/usr/share/icons/hicolor/24x24/apps', ['share/icons/hicolor/24x24/apps/safeeyes.png']),
+                ('/usr/share/icons/hicolor/48x48/apps', ['share/icons/hicolor/48x48/apps/safeeyes.png']),
+                ('/usr/share/icons/hicolor/32x32/apps', ['share/icons/hicolor/32x32/apps/safeeyes.png']),
+                ('/usr/share/icons/hicolor/64x64/apps', ['share/icons/hicolor/64x64/apps/safeeyes.png']),
+                ('/usr/share/icons/hicolor/128x128/apps', ['share/icons/hicolor/128x128/apps/safeeyes.png']),
+                ('/usr/share/icons/hicolor/48x48/status', ['share/icons/hicolor/48x48/status/safeeyes_enabled.png', 'share/icons/hicolor/48x48/status/safeeyes_disabled.png']),
+                ('/usr/share/icons/hicolor/32x32/status', ['share/icons/hicolor/32x32/status/safeeyes_enabled.png', 'share/icons/hicolor/32x32/status/safeeyes_disabled.png']),
+                ('/usr/share/icons/hicolor/24x24/status', ['share/icons/hicolor/24x24/status/safeeyes_enabled.png', 'share/icons/hicolor/24x24/status/safeeyes_disabled.png', 'share/icons/hicolor/24x24/status/safeeyes_timer.png']),
+                ('/usr/share/icons/hicolor/16x16/status', ['share/icons/hicolor/16x16/status/safeeyes_enabled.png', 'share/icons/hicolor/16x16/status/safeeyes_disabled.png', 'share/icons/hicolor/16x16/status/safeeyes_timer.png'])
                 ],
     install_requires=requires,
-    extras_require={
-        'audio': ['pyaudio']},
     entry_points={'console_scripts': ['safeeyes = safeeyes.__main__:main']},
     keywords='linux utility health eye-strain safe-eyes',
     classifiers=[
