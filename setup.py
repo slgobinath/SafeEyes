e127fa906b660022cd3ba45a8375237850ce51b8
Gobinath <slgobinath@gmail.com>
2019-03-26 06:01:43 -0400

    Prepapre for v2.0.9

--- setup.py
+++ setup.py
@@ -71,14 +71,14 @@ __data_files = list(_data_files('share'))
 
 setuptools.setup(
     name="safeeyes",
-    version="2.0.8",
+    version="2.0.9",
     description="Protect your eyes from eye strain using this continuous breaks reminder.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Gobinath Loganathan",
     author_email="slgobinath@gmail.com",
     url="https://github.com/slgobinath/SafeEyes",
-    download_url="https://github.com/slgobinath/SafeEyes/archive/v2.0.8.tar.gz",
+    download_url="https://github.com/slgobinath/SafeEyes/archive/v2.0.9.tar.gz",
     packages=setuptools.find_packages(),
     package_data={'safeeyes': __package_data()},
     data_files=__data_files,
