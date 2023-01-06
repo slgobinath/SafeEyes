375bd9d737134f61b8d4938345f291d1f99c18e1
Gobinath Loganathan <slgobinath@gmail.com>
2023-01-05 19:11:06 -0500

    Bump the version to 2.1.5

--- setup.py
+++ setup.py
@@ -78,14 +78,14 @@ def __package_data():
 
 setuptools.setup(
     name="safeeyes",
-    version="2.1.4",
+    version="2.1.5",
     description="Protect your eyes from eye strain using this continuous breaks reminder.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Gobinath Loganathan",
     author_email="slgobinath@gmail.com",
     url="https://github.com/slgobinath/SafeEyes",
-    download_url="https://github.com/slgobinath/SafeEyes/archive/v2.1.4.tar.gz",
+    download_url="https://github.com/slgobinath/SafeEyes/archive/v2.1.5.tar.gz",
     packages=setuptools.find_packages(),
     package_data={'safeeyes': __package_data()},
     data_files=__data_files(),
