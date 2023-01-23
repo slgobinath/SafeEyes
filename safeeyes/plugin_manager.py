788e2b43af4c1fb6d49714234ad026caf4e757b9
AdamPS <5490095+AdamPS@users.noreply.github.com>
2023-01-23 15:51:52 +0000

    Improve error message to include information from the exception

--- safeeyes/plugin_manager.py
+++ safeeyes/plugin_manager.py
@@ -84,8 +84,8 @@ class PluginManager:
         for plugin in config.get('plugins'):
             try:
                 self.__load_plugin(plugin)
-            except BaseException:
-                logging.error('Error in loading the plugin: %s', plugin['id'])
+            except BaseException as e:
+                logging.error('Error in loading the plugin %s: %s', plugin['id'], e)
                 continue
         # Initialize the plugins
         for plugin in self.__plugins_on_init:
