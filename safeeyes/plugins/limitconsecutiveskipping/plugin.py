3428759a85cbf9d0182b0df92d28c35b8d34e41d
undefiened <undefiened@gmail.com>
2023-05-28 21:07:55 +0200

    Fixed comment

--- safeeyes/plugins/limitconsecutiveskipping/plugin.py
+++ safeeyes/plugins/limitconsecutiveskipping/plugin.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-Show health statistics on the break screen.
+Limit how many breaks can be skipped or postponed in a row.
 """
 
 import logging
