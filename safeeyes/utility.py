49ce95530349bc4d7550730f99cdebf81c2d091c
Ilario Gelmetti <iochesonome@gmail.com>
2023-07-07 11:50:42 +0200

    utility: initialize_safeeyes don't call create_startup_entry

--- safeeyes/utility.py
+++ safeeyes/utility.py
@@ -391,8 +391,6 @@ def initialize_safeeyes():
         shutil.copy2(SYSTEM_STYLE_SHEET_PATH, STYLE_SHEET_PATH)
         os.chmod(STYLE_SHEET_PATH, 0o777)
 
-    create_startup_entry()
-
 
 def create_startup_entry():
     """
