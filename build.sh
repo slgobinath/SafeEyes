75454b8313cdedccb4c9daf261f10bda784de8c8
Tero Paloheimo <tero.paloheimo@iki.fi>
2023-12-30 20:07:42 +0200

    Use recommended command in build.sh
    The use of setup.py as a command line tool is deprecated.
    build.sh is updated to use the recommended building command.

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 rm -rf build/ dist/ safeeyes.egg-info/ .eggs/
 
-python3 setup.py sdist bdist_wheel
+pip3 install build
+python3 -m build
 twine upload --repository pypitest dist/safeeyes*.tar.gz
 clear >$(tty)
 twine upload --repository pypitest dist/safeeyes*.whl
