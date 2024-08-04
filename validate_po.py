1db2fdd4874dc52e7ea717c9c2304762b0b3e524
deltragon <m@dafert.at>
2024-08-04 18:40:04 +0200

    translations: more exact validation of po files

--- validate_po.py
+++ validate_po.py
@@ -17,20 +17,49 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from collections import defaultdict
 import os
 import polib
+import re
 import sys
 
+def has_equal_placeholders(left: str, right: str) -> bool:
+    percents = re.finditer(r'%(?P<name>\(\w+\))?(?P<format>[a-z])', left)
+
+    unnamed = defaultdict(int)
+    named = []
+    for percent in percents:
+        if percent.group('name'):
+            named.append(f"%({percent.group('name')}){percent.group('format')}")
+        else:
+            match = f"%{percent.group('format')}"
+            unnamed[match] += 1
+
+    # count unnamed cases (eg. %s, %d)
+    for match, count in unnamed.items():
+        if right.count(match) != count:
+            return False
+
+    # no need to count named cases - they are optional
+
+    return True
+
 def validate_po(locale: str, path: str) -> bool:
-     success = True
-     po = polib.pofile(path)
-     for entry in po:
-         if entry.msgstr and (entry.msgid.count("%") != entry.msgstr.count("%")):
-             print("Number of variables mismatched in " + locale)
-             print(entry.msgid + " -> " + entry.msgstr)
-             print()
-             success = False
-     return success
+    success = True
+    po = polib.pofile(path)
+    for entry in po:
+        if entry.msgstr and not has_equal_placeholders(entry.msgid, entry.msgstr):
+            print("Number of variables mismatched in " + locale)
+            print(entry.msgid + " -> " + entry.msgstr)
+            print()
+            success = False
+        for plural in entry.msgstr_plural.values():
+            if plural and not has_equal_placeholders(entry.msgid, plural):
+                print("Number of variables mismatched in " + locale)
+                print(entry.msgid + " -> " + plural)
+                print()
+                success = False
+    return success
 
 success = True
 locales = os.listdir('safeeyes/config/locale')
@@ -39,4 +68,5 @@ for locale in sorted(locales):
     if os.path.isfile(path):
         print('Validating translation %s...' % path)
         success = validate_po(locale, path) and success
+
 sys.exit(0 if success else 1)
