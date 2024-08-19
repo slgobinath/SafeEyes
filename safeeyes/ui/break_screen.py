d1b42481c6fbdf6188ec084613548b4f76f1d26d
deltragon <m@dafert.at>
2024-08-19 15:50:27 +0200

    typing: mypy passes

--- safeeyes/ui/break_screen.py
+++ safeeyes/ui/break_screen.py
@@ -24,7 +24,7 @@ import time
 import gi
 from safeeyes import utility
 from Xlib.display import Display
-from Xlib.display import X
+from Xlib import X
 
 gi.require_version("Gtk", "4.0")
 from gi.repository import Gdk
