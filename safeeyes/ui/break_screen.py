bff68d7b8990935d4a87c6a87ebdd0fd73539402
m00nwtchr <m00nwtchr@duck.com>
2024-12-17 20:12:41 +0100

    Remove dependency on X11 from break_screen

--- safeeyes/ui/break_screen.py
+++ safeeyes/ui/break_screen.py
@@ -44,7 +44,7 @@ class BreakScreen:
     def __init__(self, context, on_skipped, on_postponed, style_sheet_path):
         self.context = context
         self.count_labels = []
-        self.display = Display()
+        self.display = None
         self.enable_postpone = False
         self.enable_shortcut = False
         self.is_pretified = False
@@ -55,6 +55,10 @@ class BreakScreen:
         self.shortcut_disable_time = 2
         self.strict_break = False
         self.windows = []
+        self.is_wayland = os.environ.get('XDG_SESSION_TYPE') == 'wayland'
+
+        if not self.is_wayland:
+            self.display = Display()
 
         # Initialize the theme
         css_provider = Gtk.CssProvider()
@@ -131,7 +135,8 @@ class BreakScreen:
         Hide the break screen from active window and destroy all other windows
         """
         logging.info("Close the break screen(s)")
-        self.__release_keyboard()
+        if not self.is_wayland:
+            self.__release_keyboard()
 
         # Destroy other windows if exists
         GLib.idle_add(lambda: self.__destroy_all_screens())
@@ -149,7 +154,11 @@ class BreakScreen:
         Show an empty break screen on all screens.
         """
         # Lock the keyboard
-        utility.start_thread(self.__lock_keyboard)
+        if not self.is_wayland:
+            utility.start_thread(self.__lock_keyboard)
+        else:
+            # TODO: Wayland keyboard locking
+            logging.warning("Keyboard locking not yet implemented for Wayland.")
 
         display = Gdk.Display.get_default()
         screen = display.get_default_screen()
@@ -243,8 +252,12 @@ class BreakScreen:
 
     def __lock_keyboard(self):
         """
-        Lock the keyboard to prevent the user from using keyboard shortcuts
+        Lock the keyboard to prevent the user from using keyboard shortcuts (X11 only)
         """
+        if self.is_wayland:
+            logging.warning("Keyboard locking is not supported in Wayland.")
+            return
+
         logging.info("Lock the keyboard")
         self.lock_keyboard = True
 
@@ -273,6 +286,9 @@ class BreakScreen:
         """
         Release the locked keyboard.
         """
+        if self.is_wayland:
+            return
+
         logging.info("Unlock the keyboard")
         self.lock_keyboard = False
         self.display.ungrab_keyboard(X.CurrentTime)
